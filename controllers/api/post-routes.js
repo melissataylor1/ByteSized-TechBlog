11f30375b4b2de3f1659b5bf834c0a3041a2651b
m1taylor <m1taylor@ryerson.ca>
2023-03-10 15:56:33 -0500

    get route for editing post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -17,3 +17,23 @@ router.post('/', withAuth, async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+//post route for editing blog post
+router.put('/:id', async (req, res) => {
+    try {
+      const blogData = await Post.update({
+        title: req.body.title,
+        content: req.body.content,
+        user_id: req.session.user_id,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.status(200).json(blogData);
+    } catch (err) {
+        res.status(500).json(err);
+      };
+  });
+  
