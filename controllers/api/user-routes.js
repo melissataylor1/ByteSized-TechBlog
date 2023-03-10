917295cc80ab615f468be350c7ae6f5a6944551a
m1taylor <m1taylor@ryerson.ca>
2023-03-10 15:51:14 -0500

    logout post route for users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -54,3 +54,17 @@ router.post('/login', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// POST route for user logoit
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
+  
