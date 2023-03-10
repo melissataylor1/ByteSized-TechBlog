afc266f3ddccde2f2ac2d2081ae612206f26fd47
m1taylor <m1taylor@ryerson.ca>
2023-03-10 15:08:22 -0500

    added connection

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,22 @@
+const Sequelize = require('sequelize');
+//login data from .env file
+require('dotenv').config();
 
-DB_NAME='blog_db'
-DB_USER='root'
-DB_PASSWORD='pass' 
+let sequelize;
+//JAWSDB is heroku 
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+module.exports = sequelize;
