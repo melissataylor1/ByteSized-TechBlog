54baf489b729550e8b0dc9fdbff52558adee0f8b
m1taylor <m1taylor@ryerson.ca>
2023-03-10 15:34:48 -0500

    added get route for indiviual post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,21 +17,41 @@ router.get('/login', (req, res) => {
 router.get('/', async (req, res) => {
     //find all posts
     try {
-        const blogData = await Blog.findAll({
-            include: [
-                {
-                    model: User,
-                    attributes:(['name'], ['date_created'])
-                },
-            ],
-        });
+        const blogData = await Post.findAll({
+            include: [{
+                model: User,
+                attributes: ( ['username'], ['date_created'])
+              }],
+          });
         // passes blog data and login sessions into homepage
-        const blogs = blogData.map((blog) => blog.get({ plain: true }));
+        const posts = blogData.map((post) => post.get({ plain: true }));
         res.render('homepage', { 
-            blogs, 
+            posts, 
             logged_in: req.session.logged_in 
         });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+    });
+
+// GET singular post
+router.get('/post/:id', async (req, res) => {
+    try {
+        const blogData = await Post.findByPk(req.params.id, {
+            include: [{
+                model: Comment,
+                include: [{
+                  model: User,
+                }]
+              }, { model: User }],
+            });
+
+        const post = blogData.get({ plain: true });
+
+        res.render('post', { post, loggedIn: req.session.loggedIn, commenter: "Commenter" });
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
