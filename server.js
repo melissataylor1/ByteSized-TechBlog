9dabc9276858bc447a55eb4ba56743d8e28b9de4
m1taylor <m1taylor@ryerson.ca>
2023-03-10 14:41:26 -0500

    sql sync

--- server.js
+++ server.js
@@ -33,3 +33,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+
+//sql connect
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('ALL GOOD Now listening'));
+  });
