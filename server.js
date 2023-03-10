68f88c6eb2758dfa0b353106e122589e84676da1
m1taylor <m1taylor@ryerson.ca>
2023-03-10 14:39:35 -0500

    express session set up

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const express = require('express');
 // Import express-session
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+// express set up
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
 
 // Internal imports
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');\
+
