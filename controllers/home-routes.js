db3f8d16751643e13c52536c9c6dd67ad3c67455
m1taylor <m1taylor@ryerson.ca>
2023-03-10 15:27:17 -0500

    troubleshooting home route homepage get

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,24 +2,6 @@
 const router = require('express').Router();
 const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
-
-
-// GET route for homepage (this)
-router.get('/', async (req, res) => {
-    try {
-      const dbPostData = await Post.findAll({
-        include: [{
-            model: User,
-            attributes:(['username'],['date_created'])
-          }],
-      });
-      const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render('homepage', { posts, loggedIn: req.session.loggedIn });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
   
  // signup GET route
 router.get('/signup', (req, res) => {
@@ -31,3 +13,25 @@ router.get('/login', (req, res) => {
     res.render('login', { loggedIn: req.session.loggedIn });
   });
   
+  // GET route for homepage
+router.get('/', async (req, res) => {
+    //find all posts
+    try {
+        const blogData = await Blog.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes:(['name'], ['date_created'])
+                },
+            ],
+        });
+        // passes blog data and login sessions into homepage
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
+        res.render('homepage', { 
+            blogs, 
+            logged_in: req.session.logged_in 
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
