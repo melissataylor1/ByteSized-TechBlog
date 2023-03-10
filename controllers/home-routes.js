eebc30201bc7bd60f0e99b7a5c199bb5d6ce97f4
m1taylor <m1taylor@ryerson.ca>
2023-03-10 15:22:01 -0500

    get route of homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,3 +2,22 @@
 const router = require('express').Router();
 const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
+
+
+// GET route for homepage
+router.get('/', async (req, res) => {
+    try {
+      const dbPostData = await Post.findAll({
+        include: [{
+            model: User,
+            attributes:(['username'],['date_created'])
+          }],
+      });
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+  
