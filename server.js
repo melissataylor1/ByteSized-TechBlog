8304a6af938bd4e5b6e2de9bd42eda40d3bc2a92
m1taylor <m1taylor@ryerson.ca>
2023-03-10 14:40:36 -0500

    added middleware

--- server.js
+++ server.js
@@ -7,11 +7,29 @@ const exphbs = require('express-handlebars');
 // express set up
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-
+//express-session set up 
+const sess = {
+    secret: 'Super secret secret',
+    resave: false,
+    saveUninitialized: true,
+  };
+  
 
 // Internal imports
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');\
+const helpers = require('./utils/helpers');
 
+// Middleware
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
