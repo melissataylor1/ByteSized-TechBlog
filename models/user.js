9332f74199d03d4307ecb084bc6c96703eb479d9
m1taylor <m1taylor@ryerson.ca>
2023-03-12 14:04:47 -0400

    user table

--- models/user.js
+++ models/user.js
@@ -8,3 +8,27 @@ class User extends Model {
       return bcrypt.compareSync(loginPw, this.password); // Instance method to check password (user input vs database)
     }
   }
+  
+//model for user table in SQL library
+  User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [8], // Minimum length of 8 characters
+        },
+      },
+    },
+  )
