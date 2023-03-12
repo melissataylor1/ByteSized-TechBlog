deeeab2641a7aa4454ab58aaf31fa852f4b1efd2
m1taylor <m1taylor@ryerson.ca>
2023-03-12 14:16:24 -0400

    leaves comment with post request

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,6 +1,23 @@
 //retrieve post id
 const post_id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1];
+ 
+    
+//leaves new comment with get reuwest
+const newcommentFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const content = document.querySelector('#content-newcomment').value.trim();
+  
+    if (content) {
+      const response = await fetch(`/api/comment`, {
+        method: 'POST',
+        body: JSON.stringify({ content, post_id }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+
+  }; 
   
 //event listeners
 document
