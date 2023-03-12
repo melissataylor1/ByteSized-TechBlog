a139353cc600100d05aa4ef78d9424d9e9aa546d
m1taylor <m1taylor@ryerson.ca>
2023-03-12 14:33:52 -0400

    helper for date format

---
+++ utils/helpers.js
@@ -0,0 +1,7 @@
+//date format helper
+module.exports = {
+    format_date: (date) => {
+      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    },
+  };
+   
