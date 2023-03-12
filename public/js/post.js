78a547fe721d311865c6b9a87acf21c29895cacd
m1taylor <m1taylor@ryerson.ca>
2023-03-12 14:20:41 -0400

    create new post js

--- public/js/post.js
+++ public/js/post.js
@@ -1,4 +1,20 @@
-
+// post request for adding new blog post
+const newpostFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#title-newpost').value.trim();
+    const content = document.querySelector('#content-newpost').value.trim();
+  
+    if (title && content) {
+      const response = await fetch('/api/post', {
+        method: 'POST',
+        body: JSON.stringify({ title, content }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+     
+    }
+  };
   // Event listeners
   document
     .querySelector('.newpost-form')
