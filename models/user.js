ea1d01d7f83a6ddf4bfa51414ad7d7d89d72e358
m1taylor <m1taylor@ryerson.ca>
2023-03-12 14:03:48 -0400

    added class method to user model

--- models/user.js
+++ models/user.js
@@ -1,3 +1,10 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
+
+//class method of the User model.
+class User extends Model {
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password); // Instance method to check password (user input vs database)
+    }
+  }
