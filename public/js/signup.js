b7f715064a138a088735147d2ae71cef476b84c9
m1taylor <m1taylor@ryerson.ca>
2023-03-12 14:18:31 -0400

    post request for new user with if else for error

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,4 +1,27 @@
-
+// Signup request
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    //post request for new usr
+    if (username && password) {
+      const response = await fetch('/api/user', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/'); 
+      } else {
+        alert('Sign up failed. Try again.'); 
+      }
+    }
+  };
+  
+  
   // Event listeners
   document
     .querySelector('.signup-form')
