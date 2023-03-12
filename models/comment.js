006fb70155cae6c9db63615669e5b7ca21c475db
m1taylor <m1taylor@ryerson.ca>
2023-03-12 13:47:22 -0400

    initialized comment model within SQL, defining columns of comment table

--- models/comment.js
+++ models/comment.js
@@ -1,2 +1,33 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+
+
+Comment.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      post_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'post',
+          key: 'id',
+        },
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+    },
+);
