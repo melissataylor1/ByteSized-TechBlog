cb7f51cf6b5836469e92b9e2692896bfba7eec4f
m1taylor <m1taylor@ryerson.ca>
2023-03-12 14:32:55 -0400

    edited seeds for unique passwords

--- seeds/user-data.js
+++ seeds/user-data.js
@@ -3,19 +3,19 @@ const { User } = require('../models');
 const userdata = [
   {
     username: 'user1',
-    password: 'password',
+    password: 'pass1',
   },
   {
     username: 'user2',
-    password: 'password',
+    password: 'pass2',
   },
   {
     username: 'user3',
-    password: 'password',
+    password: 'pass3',
   },
   {
     username: 'user4',
-    password: 'password',
+    password: 'pass4',
   },
 ];
 
