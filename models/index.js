959a7cb62998c4685c37944f4a9916dba176b5fb
m1taylor <m1taylor@ryerson.ca>
2023-03-12 13:55:09 -0400

    define foreign keys for each relationship in index (post comment user)

--- models/index.js
+++ models/index.js
@@ -1,3 +1,32 @@
 const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
+//user and post
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+  });
+  
+  Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+  
+ //user and comment
+  User.hasMany(Comment, {
+    foreignKey: 'user_id',
+  });
+  
+  Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+  
+//post and comment
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+  });
+  
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+  });
+  
+  module.exports = { User, Post, Comment };
+  
