f07b857e3827d7cac41cfc71af186d2a9436f65a
m1taylor <m1taylor@ryerson.ca>
2023-03-13 17:33:25 -0400

    troubleshooting

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,15 @@
-const Sequelize = require('sequelize');
-//login data from .env file
 require('dotenv').config();
 
-let sequelize;
-//JAWSDB is heroku 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const sequelize = require('../config/connection');
-const seedUsers = require('./userData');
-const seedPosts = require('./postData');
-const seedComments = require('./commentData');
+const seedUsers = require('./user-data');
+const seedPosts = require('./post-data');
+const seedComments = require('./comment-data');
 //using the Sequelize library to create a database and seed it with data
 const seedAll = async () => {
   await sequelize.sync({ force: true });
