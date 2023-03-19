bc96858b33bcc65155b634f3d6849d792a8e7844
melissataylor1 <melissataylor@Melissas-MacBook-Pro.local>
2023-03-19 18:13:30 -0400

    fixed home route controller

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,92 +1,84 @@
-//middleware
 const router = require('express').Router();
-const { User, Post } = require('../models');
-const withAuth = require('../utils/auth');
-  
- // signup GET route
-router.get('/signup', (req, res) => {
-    res.render('signup');
-  });
-  
-// loin GET route
-router.get('/login', (req, res) => {
-    res.render('login');
-  });
-  
-  // GET route for homepage
-router.get('/', async (req, res) => {
-    //find all posts
-    try {
-        const blogData = await Post.findAll({
-            include: [{
+const { Post, User, Comment } = require('../models');
+const sequelize = require('../config/connection');
+
+router.get('/', (req, res) => {
+    console.log(req.session);
+
+    Post.findAll({
+        attributes: ['id', 'post_text', 'title', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
                 model: User,
-                attributes: ( ['username'])
-              }],
-          });
-        // passes blog data and login sessions into homepage
-        const posts = blogData.map((post) => post.get({ plain: true }));
-        res.render('home');
-    } catch (err) {
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(postData => {
+        const posts = postData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
         console.log(err);
         res.status(500).json(err);
-      }
     });
-
-    // GET for panel page
-router.get('/panel', async (req, res) => { // withAuth: only if user is logged in, the callback function is executed
-    try {
-      const blogData = await Post.findAll({
-        include: [{
-          model: User,
-          attributes: ['username'],
-        }]
-       /*() where: {
-          user_id: req.session.user_id,
-        },*/
-      });
-      const posts = blogData.map((post) => post.get({ plain: true }));
-      res.render('panel');
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
-  
-// GET singular post
-router.get('/post/:id', async (req, res) => {
-    try {
-        const blogData = await Post.findByPk(req.params.id, {
-            include: [{
-                model: Comment,
-                include: [{
-                  model: User,
-                }]
-              }, { model: User }],
-            });
-
-        const post = blogData.get({ plain: true });
-
-        res.render('post');
-    } catch (err) {
-        res.status(500).json(err);
-    }
 });
 
-// GET for adding/deleting/editing posts
-// withAuth middleware preventing route access unless logged in 
-router.get('/panel/new', withAuth, (req, res) => { 
-    res.render('new');
-  });
-
-  router.get('/panel/post/:id', withAuth, async (req, res) => {
-    try {
-      const blogData = await Post.findByPk(req.params.id, {});
-      const post = blogData.get({ plain: true });
-      res.render('edit');
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+router.get('/login', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return;
     }
-  });
+    res.render('login');
+});
 
-  module.exports = router;
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_text', 'title', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                  }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(postData => {
+        if (!postData) {
+            res.status(404).json({ message: 'No post found' });
+            return;
+        }
+
+        const post = postData.get({ plain: true });
+
+        res.render('singlepost', { post, loggedIn: req.session.loggedIn});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
