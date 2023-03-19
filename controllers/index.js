693880daaac2271eba7303fe01fbbc46a6b56ad2
melissataylor1 <melissataylor@Melissas-MacBook-Pro.local>
2023-03-19 18:12:48 -0400

    added dash controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
+const homeRoutes = require('./homeRoute.js');
+const dashboardRoutes = require('./dashboardRoute');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
