e79e0a116890b43047550ac8100efe635b2fd510
m1taylor <m1taylor@ryerson.ca>
2023-03-21 17:29:35 -0400

    troubleshoot home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,83 +3,61 @@ const router = express.Router();
 const { Blog, User, Comment } = require('../models');
 
 // GET route for homepage
-router.get('/', (req, res) => {
-    console.log(req.session);
 
-    Post.findAll({
-        attributes: ['id', 'post_text', 'title', 'created_at'],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-    .then(postData => {
-        const posts = postData.map(post => post.get({ plain: true }));
-        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+router.get('/', async (req, res) => {
+    Blog.findAll({include:[User,Comment]}).then(blogs=>{
+        //outputs the blog entries from the database as Sequelize objects
+        const blogsHbsData = blogs.map(blog=>blog.get({plain:true}))
+        console.log(blogs);
+        console.log("==============")
+        console.log(blogsHbsData) 
+        //rendering 'homepage' view and passing in:
+        res.render('homepage', {
+            //1. an array of blog posts
+            blogs:blogsHbsData,
+            //2. boolean value that indicates whether the user is logged in or not
+            loggedIn: req.session.loggedIn,
+            //3. the user's unique ID if they are logged in
+            user_id: req.session.user_id
+        })
+        })
 });
+
+// GET route for login 
 
 router.get('/login', (req, res) => {
     if(req.session.loggedIn) {
-        res.redirect('/');
-        return;
+        return res.redirect('/dashboard')
     }
-    res.render('login');
+    res.render('login', {
+        loggedIn: false,
+        user_id: null
+      })
 });
-
+/* GET route for signup
 router.get('/signup', (req, res) => {
     res.render('signup');
-});
+});*/
 
-router.get('/post/:id', (req, res) => {
-    Post.findOne({
-        where: {
-            id: req.params.id
-        },
-        attributes: ['id', 'post_text', 'title', 'created_at'],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                  }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-    .then(postData => {
-        if (!postData) {
-            res.status(404).json({ message: 'No post found' });
-            return;
-        }
+//GET route for dash page with posts
+router.get('/dashboard', async (req, res) => {
+    try {
+		const userData = await User.findByPk(req.session.user_id, {
+			include: [Blog],
+		});
 
-        const post = postData.get({ plain: true });
+		const user = userData.get({
+			plain: true
+		});
 
-        res.render('singlepost', { post, loggedIn: req.session.loggedIn});
-    })
-    .catch(err => {
+		res.render('dashboard', {
+			...user,
+			loggedIn: true
+		});
+    } catch(err) {
         console.log(err);
         res.status(500).json(err);
-    });
+    }
 });
 
 module.exports = router;
