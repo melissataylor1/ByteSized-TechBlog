a4091dd5683bf974a2e8e31fd467ad3d769df8e4
m1taylor <m1taylor@ryerson.ca>
2023-03-21 23:03:03 -0400

    force false

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`))
 });
